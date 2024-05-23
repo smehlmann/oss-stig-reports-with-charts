6712e888eb0287efdae237926ef312dc680d7f53
smehlmann <sandra.mehlmann@gmail.com>
2024-05-23 14:15:54 -0400

    make Dashborad tab visible only for specific reports

--- src/components/tabs/TabsComponent.jsx
+++ src/components/tabs/TabsComponent.jsx
@@ -1,12 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import OssStigReportsTab from "./OssStigReportsTab";
 import DashboardTab from "./DashboardTab";
-import './TabComponentStyles.css'
+import "./TabComponentStyles.css";
+import useLocalStorageListener from "../useLocalStorageListener.js";
 
 import * as Tabs from "@radix-ui/react-tabs";
 
 const TabsComponent = () => {
-  const [selectedIndex, setSelectedIndex] = useState(0);
+  //const [selectedIndex, setSelectedIndex] = useState(0);
 
   /*const handleTabChange = (index) => {
     setSelectedIndex(index);
@@ -20,20 +21,66 @@ const TabsComponent = () => {
     setActiveTab(value);
   };*/
 
+  const [disableDashboard, setDisableDashboard] = useState(true);
+
+  useEffect(() => {
+    console.log("BarChartBuilder from useEffect");
+    if (localStorage.getItem("ossStigReport")) {
+      setDisableDashboard(false);
+    }
+  }, []);
+
+  useLocalStorageListener((event) => {
+    console.log("hi from useLocalStorageListener");
+    if (event.type === "storage") {
+      console.log("hi from useLocalStorageListener");
+      /*if (localStorage.getItem("ossStigReport")) {
+        setDisableDashboard(false);
+      } else {
+        setDisableDashboard(true);
+      }*/
+      if (localStorage.getItem("ossStigReport")) {
+        const selectedReport = localStorage.getItem("selectedReport");
+        if (
+          selectedReport === "5" ||
+          selectedReport === "7" ||
+          selectedReport === "8"
+        ) {
+          setDisableDashboard(false);
+        } else {
+          setDisableDashboard(true);
+        }
+      } else {
+        setDisableDashboard(true);
+      }
+    }
+  });
+
   const handleTabChange = (value) => {
     setActiveTab(value);
   };
 
   return (
-    <Tabs.Root defaultValue="tab1" className='tabs-root' onValueChange={handleTabChange}>
-      <Tabs.List aria-label="Tabs" className='tabs-list'>
-        <Tabs.Trigger className='tabs-trigger' value="tab1">Report Generator</Tabs.Trigger>
-        <Tabs.Trigger className='tabs-trigger' value="tab2">Report Dashboard</Tabs.Trigger>
+    <Tabs.Root
+      defaultValue="tab1"
+      className="tabs-root"
+      onValueChange={handleTabChange}
+    >
+      <Tabs.List aria-label="Tabs" className="tabs-list">
+        <Tabs.Trigger className="tabs-trigger" value="tab1">
+          Report Generator
+        </Tabs.Trigger>
+        {!disableDashboard && (
+          <Tabs.Trigger className="tabs-trigger" value="tab2">
+            Report Dashboard
+          </Tabs.Trigger>
+        )}
       </Tabs.List>
       <div>
         <div style={{ display: activeTab === "tab1" ? "block" : "none" }}>
           <OssStigReportsTab />
         </div>
+
         <div style={{ display: activeTab === "tab2" ? "block" : "none" }}>
           <DashboardTab />
         </div>
